2958c56f7dcf6036f7a39e9bd3b6940e319c6106
John Gratton <jgratton@gmail.com>
2018-04-08 16:59:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken(board, i)
-  if board[i] == " " || board[i] == "" || board[i] == nil
+  if board[i] == " "
     return false
   end
 end
