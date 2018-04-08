d5df1f87f2d7d6bbda323512ff4b414e8750f488
John Gratton <jgratton@gmail.com>
2018-04-08 16:52:32 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
-def position_taken(position)
-  if position == " "
+def position_taken(board, index)
+  if index == " " || index == "" || index == nil
     return false
   end
 end
